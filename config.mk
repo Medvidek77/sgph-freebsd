7313db75f0c6a0c06eeaf88a8d99e5df6851083d
Lukáš Vaverka <medvidek77@centrum.cz>
2024-08-17 16:32:57 +0200

    Delete config.mk

--- config.mk
+++
@@ -1,9 +0,0 @@
-VERSION   = 0
-
-CPPFLAGS  = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_XOPEN_SORCE=700 -D_BSD_SOURCE
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS   = -s
-
-PREFIX = /usr/local
-
-CC        = cc
